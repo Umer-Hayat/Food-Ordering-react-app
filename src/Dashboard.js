8b2d513ab45c4ddc018b63c5f3b83e66bdefc034
umer33 <umerhayat520@gmail.com>
2020-08-06 14:29:42 +0500

    master

---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './pages/Dashboard';
+
+function Dashboard() {
+    return (
+        <Dashboard />
+    );
+}
+
+export default Dashboard;
